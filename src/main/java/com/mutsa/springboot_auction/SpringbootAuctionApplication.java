a257b7ce8cb2dc6a391bdaf432939c308ff13338
gtaeu <gtaeu1201@gmail.com>
2025-09-27 20:26:21 +0900

    init

---
+++ src/main/java/com/mutsa/springboot_auction/SpringbootAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.mutsa.springboot_auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mutsa/springboot_auction/SpringbootAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutsa.springboot_auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
