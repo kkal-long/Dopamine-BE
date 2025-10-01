aff68e90829f7f2d0cab0fbea4bf8d2c33e264ef
haha4825 <tw086219@gmail.com>
2025-10-01 11:49:58 +0900

    [fix] 불필요한 설정 제거, yml파일 환경변수를 이용하도록 변경

--- src/test/java/com/mutsa/springboot_auction/SpringbootAuctionApplicationTests.java
+++ src/test/java/com/mutsa/springboot_auction/SpringbootAuctionApplicationTests.java
@@ -3,12 +3,11 @@ package com.mutsa.springboot_auction;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class SpringbootAuctionApplicationTests {
-
 	@Test
-	@Disabled("CI 환경 테스트 비활성화")
 	void contextLoads() {
 	}
 
