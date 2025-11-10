61b8121426f8a7d35c1fa8ea7cb2a81ac9588aa4
gtaeu <gtaeu1201@gmail.com>
2025-11-10 15:40:57 +0900

    auctionImage 도메인 추가

---
+++ src/main/java/com/mutsa/springboot_auction/domain/auctionImage/entity/AuctionImage.java
@@ -0,0 +1,26 @@
+package com.mutsa.springboot_auction.domain.auctionImage.entity;
+
+import com.mutsa.springboot_auction.domain.auction.entity.Auction;
+import com.mutsa.springboot_auction.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "AuctioImage")
+public class AuctionImage extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "auctionId", nullable = false)
+    private Auction auction;
+}
