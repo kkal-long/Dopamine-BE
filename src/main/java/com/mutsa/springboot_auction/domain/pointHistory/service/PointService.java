5b8600f29344da31d3e316d283413df2922a1c81
gtaeu <gtaeu1201@gmail.com>
2025-11-15 02:46:00 +0900

    포인트 내역 메서드 추가

--- src/main/java/com/mutsa/springboot_auction/domain/pointHistory/service/PointService.java
+++ src/main/java/com/mutsa/springboot_auction/domain/pointHistory/service/PointService.java
@@ -1,5 +1,6 @@
 package com.mutsa.springboot_auction.domain.pointHistory.service;
 
+import com.mutsa.springboot_auction.domain.pointHistory.entity.HistoryType;
 import com.mutsa.springboot_auction.domain.pointHistory.entity.PointHistory;
 import com.mutsa.springboot_auction.domain.pointHistory.repository.PointHistoryRepository;
 import com.mutsa.springboot_auction.domain.user.entity.User;
@@ -27,6 +28,48 @@ public class PointService {
                 .user(user)
                 .changeAmount(amount)
                 .type(CHARGE)
+                .build();
+        pointHistoryRepository.save(history);
+    }
+
+    // user랑 depositAmount 받아서 입찰시 포인트 내역에 저장
+    @Transactional
+    public void saveDepositHistory(User user, Integer depositAmount) {
+        PointHistory history = PointHistory.builder()
+                .user(user)
+                .type(HistoryType.BID_DEPOSIT)
+                .changeAmount(-depositAmount)
+                .build();
+        pointHistoryRepository.save(history);
+    }
+
+    // user랑 refundAmount 받아서 보증금 환불시 포인트 내역에 저장
+    @Transactional
+    public void saveRefundHistory(User user, Integer refundAmount) {
+        PointHistory history = PointHistory.builder()
+                .user(user)
+                .type(HistoryType.REFUND)
+                .changeAmount(refundAmount)
+                .build();
+        pointHistoryRepository.save(history);
+    }
+
+    @Transactional
+    public void savePurchaseHistory(User buyer, Integer amount) {
+        PointHistory history = PointHistory.builder()
+                .user(buyer)
+                .changeAmount(-amount)
+                .type(HistoryType.PURCHASE)
+                .build();
+        pointHistoryRepository.save(history);
+    }
+
+    @Transactional
+    public void saveSaleHistory(User seller, Integer amount) {
+        PointHistory history = PointHistory.builder()
+                .user(seller)
+                .changeAmount(amount)
+                .type(HistoryType.SALE)
                 .build();
         pointHistoryRepository.save(history);
     }
