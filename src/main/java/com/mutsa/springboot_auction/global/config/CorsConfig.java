8928035b4b6e46c679f91b5b3b42ece7a1c013f0
gtaeu <gtaeu1201@gmail.com>
2025-11-16 03:25:54 +0900

    CorsConfig @Configuration 어노테이션 추가

--- src/main/java/com/mutsa/springboot_auction/global/config/CorsConfig.java
+++ src/main/java/com/mutsa/springboot_auction/global/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.mutsa.springboot_auction.global.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
+@Configuration
 public class CorsConfig {
 
     @Bean
