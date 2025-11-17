4383241e9fa9fc345e2f8c748dbfd16aec47d279
gtaeu <gtaeu1201@gmail.com>
2025-11-17 19:30:49 +0900

    카테고리 모두 조회 api 추가

--- src/main/java/com/mutsa/springboot_auction/global/config/SecurityConfig.java
+++ src/main/java/com/mutsa/springboot_auction/global/config/SecurityConfig.java
@@ -77,7 +77,8 @@ public class SecurityConfig {
                         "/chat-test.html",
                         "/api/search/**",
                         "/api/notification/**",
-                        "/api/chat/rooms/*/complete/test"
+                        "/api/chat/rooms/*/complete/test",
+                        "/api/categories"
                 ).permitAll()
 
                 // 나머지 모든 요청은 인증 필요
