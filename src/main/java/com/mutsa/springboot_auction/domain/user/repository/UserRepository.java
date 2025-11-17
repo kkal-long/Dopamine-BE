0852f0efd4e369f7b67f8a13d7f19d4faf60e141
haha4825 <tw086219@gmail.com>
2025-11-17 20:09:24 +0900

    [fix] 유저 레포지토리 불필요한 코드 제거

--- src/main/java/com/mutsa/springboot_auction/domain/user/repository/UserRepository.java
+++ src/main/java/com/mutsa/springboot_auction/domain/user/repository/UserRepository.java
@@ -6,9 +6,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findBySocialIdAndSocialType(String socialId, SocialType socialType);
-
     Optional<User> findByEmail(String email);
 }
 
