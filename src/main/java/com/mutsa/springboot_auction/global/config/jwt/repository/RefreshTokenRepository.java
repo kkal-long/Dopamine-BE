6fa140a2505b2e6d500c6f34fb081ac829c25cc1
haha4825 <tw086219@gmail.com>
2025-11-17 21:59:10 +0900

    [feat] 로그아웃 기능 개발

--- src/main/java/com/mutsa/springboot_auction/global/config/jwt/repository/RefreshTokenRepository.java
+++ src/main/java/com/mutsa/springboot_auction/global/config/jwt/repository/RefreshTokenRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByUserId(Long userId);
     Optional<RefreshToken> findByRefreshToken(String refreshToken);
+
 }
 
