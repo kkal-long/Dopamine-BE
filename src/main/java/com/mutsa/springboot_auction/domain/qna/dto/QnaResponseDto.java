4476aa9a2cecf6a1107aed2492c840a37ad2c39b
KevinHan142 <131000805+KevinHan142@users.noreply.github.com>
2025-11-17 16:49:02 +0900

    feature QnaResponseDto

---
+++ src/main/java/com/mutsa/springboot_auction/domain/qna/dto/QnaResponseDto.java
@@ -0,0 +1,29 @@
+package com.mutsa.springboot_auction.domain.qna.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class QnaResponseDto {
+
+    private Long qnaId;
+    private Long auctionId;
+
+    private Long questionUserId;
+    private String questionUserNickname;
+    private String questionContent;
+    private LocalDateTime questionCreatedAt;
+
+    private Long answerUserId;
+    private String answerUserNickname;
+    private String answerContent;
+    private LocalDateTime answerCreatedAt;
+
+}
