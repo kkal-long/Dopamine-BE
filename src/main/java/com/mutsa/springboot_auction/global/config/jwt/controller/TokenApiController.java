61c6e5695e6b3c19d932a4ca587372fb63b13e00
haha4825 <tw086219@gmail.com>
2025-11-17 20:11:35 +0900

    [feat] 액세스 토큰 재발급 코드

---
+++ src/main/java/com/mutsa/springboot_auction/global/config/jwt/controller/TokenApiController.java
@@ -0,0 +1,26 @@
+package com.mutsa.springboot_auction.global.config.jwt.controller;
+
+import com.mutsa.springboot_auction.global.config.jwt.dto.CreateAccessTokenRequest;
+import com.mutsa.springboot_auction.global.config.jwt.dto.CreateAccessTokenResponse;
+import com.mutsa.springboot_auction.global.config.jwt.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TokenApiController {
+
+    private final TokenService tokenService;
+
+    @PostMapping("/api/token")
+    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken(@RequestBody CreateAccessTokenRequest request) {
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CreateAccessTokenResponse(newAccessToken));
+    }
+}
