9a0ba5e52e4fba6c13490e1c512bf74a8c84dbd0
haha4825 <tw086219@gmail.com>
2025-11-19 18:06:09 +0900

    [fix] 첫번째 로그인인지 표시

--- src/main/java/com/mutsa/springboot_auction/global/config/oauth/service/OAuth2SuccessHandler.java
+++ src/main/java/com/mutsa/springboot_auction/global/config/oauth/service/OAuth2SuccessHandler.java
@@ -1,6 +1,8 @@
 package com.mutsa.springboot_auction.global.config.oauth.service;
 
+import com.mutsa.springboot_auction.domain.user.entity.CustomOAuth2User;
 import com.mutsa.springboot_auction.domain.user.entity.User;
+import com.mutsa.springboot_auction.domain.user.repository.UserRepository;
 import com.mutsa.springboot_auction.domain.user.service.UserService;
 import com.mutsa.springboot_auction.global.config.jwt.TokenProvider;
 import com.mutsa.springboot_auction.global.config.jwt.domain.RefreshToken;
@@ -30,19 +32,18 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private final TokenProvider tokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
     private final OAuth2AuthorizationRequestBasedOnCookieRepository authorizationRequestRepository;
-    private final UserService userService;
-
+    private final UserRepository userRepository;
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-        User user = userService.findByEmail((String) oAuth2User.getAttributes().get("email"));
+        CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+        User user = oAuth2User.getUser();
 
         String refreshToken = tokenProvider.generateToken(user, REFRESH_TOKEN_DURATION);
         saveRefreshToken(user.getId(), refreshToken);
         addRefreshTokenToCookie(request, response, refreshToken);
 
         String accessToken = tokenProvider.generateToken(user, ACCESS_TOKEN_DURATION);
-        String targetUrl = getTargetUrl(accessToken);
+        String targetUrl = getTargetUrl(accessToken,user.getId());
 
         clearAuthenticationAttributes(request, response);
 
@@ -69,9 +70,12 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         authorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
     }
 
-    private String getTargetUrl(String token) {
+    private String getTargetUrl(String token,Long userId) {
+        User user = userRepository.findById(userId).get();
+        Boolean isFirstLogin = user.getProfileImageUrl() == null;
         return UriComponentsBuilder.fromUriString(REDIRECT_PATH)
                 .queryParam("token", token)
+                .queryParam("isFirstLogin",isFirstLogin)
                 .build()
                 .toUriString();
     }
