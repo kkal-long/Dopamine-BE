44ea56cf3050bccf4d73d30b01809188d8bd6999
gtaeu <gtaeu1201@gmail.com>
2025-11-21 16:26:22 +0900

    유저 컨트롤러 로그 추가

--- src/main/java/com/mutsa/springboot_auction/domain/user/controller/UserController.java
+++ src/main/java/com/mutsa/springboot_auction/domain/user/controller/UserController.java
@@ -7,6 +7,7 @@ import com.mutsa.springboot_auction.domain.user.entity.User;
 import com.mutsa.springboot_auction.domain.user.repository.UserRepository;
 import com.mutsa.springboot_auction.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
     private final UserService userService;
 
@@ -22,6 +24,8 @@ public class UserController {
     public ResponseEntity<UserResponse> updateProfile(@RequestBody ProfileRequest request,
                                                       @AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
         User user = customOAuth2User.getUser();
+        log.info("request 정보: {}", request);
+        log.info("유저정보: {}", user);
         return ResponseEntity.ok(UserResponse.from(userService.updateProfile(request, user)));
     }
 }
