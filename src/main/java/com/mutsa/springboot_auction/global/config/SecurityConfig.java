d537ec308b1f0891f0157336284bbd03a58a0659
Junseok Ahn <rrvvy9595@gmail.com>
2025-11-22 13:02:16 +0900

    [fix] 토스 충전 오류 수정

--- src/main/java/com/mutsa/springboot_auction/global/config/SecurityConfig.java
+++ src/main/java/com/mutsa/springboot_auction/global/config/SecurityConfig.java
@@ -74,7 +74,6 @@ public class SecurityConfig {
                                 "/h2-console/**",
                                 "/login/oauth2/**",
                                 "/api/auth/**",
-                                "/api/points/**",
                                 "/api/s3/**",          // ← 패턴 변경
                                 "/static/**",
                                 "/css/**",
