4336bb3ed0a0cbb5a3d43e623a53ba8280323053
Junseok Ahn <rrvvy9595@gmail.com>
2025-11-23 21:08:59 +0900

    [FEAT] 한국 시간으로 변경

--- src/main/java/com/mutsa/springboot_auction/SpringbootAuctionApplication.java
+++ src/main/java/com/mutsa/springboot_auction/SpringbootAuctionApplication.java
@@ -1,9 +1,12 @@
 package com.mutsa.springboot_auction;
 
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -13,5 +16,10 @@ public class SpringbootAuctionApplication {
 	public static void main(String[] args) {
 		log.info("==========================================================");
 		SpringApplication.run(SpringbootAuctionApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 }
